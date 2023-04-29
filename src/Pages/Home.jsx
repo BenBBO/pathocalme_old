d1680d539ec053bbccb3fcb813ed1d4c67078951
ben <b.bouillet@gmail.com>
2023-04-29 15:38:49 +0200

    Test home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 
+import BirdsFlying from "Components/BirdsFlying";
 import "./Home.scss";
 
 export default function Home() {
@@ -6,25 +7,12 @@ export default function Home() {
     return <div id="home-page">
 
         <div id="logo-container">
-            <img alt="Pathocalme logo" obj src={require("Assets/Images/logo.png")} />
-        </div>
-        <div class="bird-container bird-container--one">
-            <div class="bird bird--one"></div>
-        </div>
-
-        <div class="bird-container bird-container--two">
-            <div class="bird bird--two"></div>
-        </div>
-
-        <div class="bird-container bird-container--three">
-            <div class="bird bird--three"></div>
-        </div>
-
-        <div class="bird-container bird-container--four">
-            <div class="bird bird--four"></div>
+            <img alt="Pathocalme logo" src={require("Assets/Images/logo.png")} />
+            <BirdsFlying />
         </div>
         <div id="amigos-container">
-            <img alt="3 chevreaux" obj src={require("Assets/Images/3amigos.jpg")} />
+            <div id="picture-container" />
+            {/* <img alt="3 chevreaux" src={require("Assets/Images/3amigos.jpg")} /> */}
         </div>
 
 
