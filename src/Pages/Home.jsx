2bed914d5eb07fa1859fa2095308a1ef1245fdb5
ben <b.bouillet@gmail.com>
2023-05-02 22:02:49 +0200

    Wip test cards

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@
 import BirdsFlying from "Components/BirdsFlying";
 import "./Home.scss";
 import Egg from "Components/Egg";
+import { Grid } from "@mui/material";
 
 export default function Home() {
 
@@ -12,11 +13,31 @@ export default function Home() {
             <BirdsFlying />
         </div>
 
-        <div id="eggs">
+        {/* <div id="eggs">
             <Egg eggColor="#a5b6a6" title="Animation" target="/animation" />
             <Egg eggColor="#bd6966" title="Médiation" target="/mediation" align="flex-end" />
             <Egg title="Pédagogie" target="/pedagogie" />
-        </div>
+        </div> */}
+        <Grid container className="home-item-container" style={{
+            backgroundColor: "white"
+        }}>
+            <Grid item xs={6} sx={{ p: 2 }}>
+                <Egg eggColor="#a5b6a6" title="Animation" target="/animation" />
+            </Grid>
+            <Grid xs={6}>
+                <img alt="animation" className="right-aligned" src={require("Assets/Images/animation.jpg")} />
+            </Grid>
+        </Grid>
+        <Grid container className="home-item-container" style={{
+            backgroundColor: "white"
+        }}>
+            <Grid item xs={6}>
+                <img alt="animation" className="left-aligned" src={require("Assets/Images/mediation.jpg")} />
+            </Grid>
+            <Grid xs={6} sx={{ p: 2 }}>
+                <Egg eggColor="#bd6966" title="Médiation" target="/mediation" />
+            </Grid>
+        </Grid>
 
-    </div>
+    </div >
 }
